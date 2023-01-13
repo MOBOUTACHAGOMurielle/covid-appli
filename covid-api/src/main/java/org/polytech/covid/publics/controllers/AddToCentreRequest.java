3e58b32f42ffe87a022182be2accf62cb6dfdcdb
wsanon <sanon427@gmail.com>
2023-01-13 20:28:27 +0100

    update

---
+++ covid-api/src/main/java/org/polytech/covid/publics/controllers/AddToCentreRequest.java
@@ -0,0 +1,12 @@
+package org.polytech.covid.publics.controllers;
+
+import lombok.Getter;
+
+@Getter
+public class AddToCentreRequest {
+  public String nom;
+  public String prenom;
+  public String email;
+  public String password;
+
+}
